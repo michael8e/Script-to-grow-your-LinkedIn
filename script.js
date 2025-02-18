ffb8025a31a5e501f8777eeef34ccac3d8d252a2
Michael <8michael8e@gmail.com>
2025-02-18 19:55:30 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var all = document.getElementsByTagName("button");
-
-for (var i=0, max=all.length; i < max; i++) {
-    if(all[i].innerText == 'Connect'){
-       all[i].click();
-        
-    }
-     // Do something with the element here
-}
